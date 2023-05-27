a1b65589c58074179d1907c20eb459666b30fddb
Thyago-Oliveira-Perez <thyagodoliveiraperez@gmail.com>
2023-05-27 14:29:03 -0300

    feat: add args to query userResolver

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,13 +1,14 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 import { User } from './models/user';
 import { UserService } from './services/user.service';
+import { GetUserArgs } from './dto/args/get-user.args';
 
 @Resolver(() => User)
 export class UserResolver {
   constructor(private readonly userService: UserService) {}
 
   @Query(() => User, { name: 'user', nullable: true })
-  async getUser(): Promise<User> {
-    return this.userService.getUser();
+  async getUser(@Args() getUserArgs: GetUserArgs): Promise<User> {
+    return this.userService.getUserById();
   }
 }
