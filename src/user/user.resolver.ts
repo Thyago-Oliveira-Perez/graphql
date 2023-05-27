e7e655d30c849d5fc12c0f1538047b04cc03cdad
Thyago-Oliveira-Perez <thyagodoliveiraperez@gmail.com>
2023-05-27 14:24:58 -0300

    feat: add user query on userResolver

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,4 +1,13 @@
-import { Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
+import { User } from './models/user';
+import { UserService } from './services/user.service';
 
-@Resolver()
-export class UserResolver {}
+@Resolver(() => User)
+export class UserResolver {
+  constructor(private readonly userService: UserService) {}
+
+  @Query(() => User, { name: 'user', nullable: true })
+  async getUser(): Promise<User> {
+    return this.userService.getUser();
+  }
+}
