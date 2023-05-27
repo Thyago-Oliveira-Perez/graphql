1f486fad1e1bbdab96d1dfb1d02eb9b3076519d4
Thyago-Oliveira-Perez <thyagodoliveiraperez@gmail.com>
2023-05-27 15:10:22 -0300

    feat: add getUsers query on userResolver with args

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -2,6 +2,7 @@ import { Args, Query, Resolver } from '@nestjs/graphql';
 import { User } from './models/user';
 import { UserService } from './services/user.service';
 import { GetUserArgs } from './dto/args/get-user.args';
+import { GetUsersArgs } from './dto/args/get-users.args';
 
 @Resolver(() => User)
 export class UserResolver {
@@ -10,5 +11,10 @@ export class UserResolver {
   @Query(() => User, { name: 'user', nullable: true })
   async getUser(@Args() getUserArgs: GetUserArgs): Promise<User> {
     return this.userService.getUserById();
+  }
+
+  @Query(() => [User], { name: 'users', nullable: 'items' })
+  async getUsers(@Args() getUsersargs: GetUsersArgs): Promise<User[]> {
+    return this.userService.getUsers();
   }
 }
